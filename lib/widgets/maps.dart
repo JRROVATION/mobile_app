23b5bcd799d11c520c46ea7c8adb107508c33deb
satriapamngkas <satriap2101@gmail.com>
2023-12-27 20:08:28 +0700

    map display

--- lib/widgets/maps.dart
+++ lib/widgets/maps.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MapsLocation extends StatefulWidget {
@@ -10,7 +13,30 @@ class MapsLocation extends StatefulWidget {
 }
 
 class _MapsLocationState extends State<MapsLocation> {
+  late MapController controller;
   @override
+  void initState() {
+    super.initState();
+    Timer.periodic(const Duration(seconds: 10), (timer) {
+      if (mounted) {
+        // _loadData();
+      }
+    });
+    controller = MapController(
+      initMapWithUserPosition: const UserTrackingOption(enableTracking: true),
+    );
+
+    controller.listenerMapSingleTapping.addListener(() {
+      if (controller.listenerMapSingleTapping.value != null) {
+        double latitude =
+            controller.listenerMapSingleTapping.value!.latitude.toDouble();
+        print(latitude);
+      }
+    });
+  }
+
+  @override
+  @mustCallSuper
   Widget build(BuildContext context) {
     return Container(
       height: 362,
@@ -18,6 +44,17 @@ class _MapsLocationState extends State<MapsLocation> {
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
         color: const Color.fromRGBO(232, 232, 232, 1),
+      ),
+      child: OSMFlutter(
+        controller: controller,
+        osmOption: const OSMOption(
+          zoomOption: ZoomOption(
+            initZoom: 17,
+            maxZoomLevel: 19,
+            minZoomLevel: 2,
+            stepZoom: 1.0,
+          ),
+        ),
       ),
     );
   }
