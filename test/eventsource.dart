2f56402152203df747f384d899f813c72a32a2a0
theunra <diandrarizqy7@gmail.com>
2024-10-17 00:45:47 +0700

    eentsour'

--- test/eventsource.dart
+++ test/eventsource.dart
@@ -3,31 +3,31 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  var url = Uri.http('10.8.0.5:5555', '/');
+  var url = Uri.http('theunra.site:5555', '/');
   var response = await http.get(url);
   print('Response status: ${response.statusCode}');
 
   final client = http.Client();
 
-  final http.StreamedResponse rstream =
-      await client.send(http.Request('GET', url.replace(path: '/sse')));
+  final http.StreamedResponse rstream = await client.send(
+      http.Request('GET', url.replace(path: '/device/hello/data/listen')));
 
   rstream.stream.listen((onData) {
     final str = utf8.decode(onData);
 
-    // print(str);
+    print(str);
 
-    final datas = str.split('\n');
+    // final datas = str.split('\n');
 
-    var id = datas[0].split(':').last;
-    var data = datas[1].split('data:').last;
+    // var id = datas[0].split(':').last;
+    // var data = datas[1].split('data:').last;
 
-    try {
-      final j = jsonDecode(data);
+    // try {
+    //   final j = jsonDecode(data);
 
-      print("id : $id\ndata : $j");
-    } on FormatException {
-      print("Format Exception");
-    }
+    //   print("id : $id\ndata : $j");
+    // } on FormatException {
+    //   print("Format Exception");
+    // }
   });
 }
